c5798ec5b05de45aa9b75eb1d41cf62a5db2430e
FaDDeZ <139443537+FaDDeZ@users.noreply.github.com>
2023-09-24 17:10:56 +0300

    Add files via upload

---
+++ Test_1/Circle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    public class Circle : Figure
+    {
+        private float R;
+        public Circle(float R)
+        {
+            if (R < 0)
+            {
+                throw new ArgumentOutOfRangeException("Arguments lower then zero");
+            }
+            this.R = R;
+            S = Square();
+        }
+        public override float Square()
+        {
+            return R * R * PI;
+        }
+    }
+}
---
+++ Test_1/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace ConsoleApp3
+{
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input name of figure (triangle or circle): ");
+            string name = Console.ReadLine();
+            float a, b, c;
+            switch (name.ToLower())
+            {
+                case "triangle":
+                    try
+                    {
+                        line();
+                        a = Convert.ToSingle(Console.ReadLine());
+                        line();
+                        b = Convert.ToSingle(Console.ReadLine());
+                        line();
+                        c = Convert.ToSingle(Console.ReadLine());
+                        try
+                        {
+                            Triangle tr = new Triangle(a, b, c);
+                            Console.WriteLine(tr.get_Square());
+                        }
+                        catch (Exception Ex)
+                        {
+                            Console.WriteLine(Ex.Message);
+                        }
+                    }
+                    catch 
+                    {
+                        Console.WriteLine("You need digits");
+                    }
+                    break;
+                case "circle":
+                    try
+                    {
+                        line();
+                        a = Convert.ToSingle(Console.ReadLine());
+                        Circle cr = new Circle(a);
+                        Console.WriteLine(cr.get_Square());
+                        
+                    }
+                    catch
+                    {
+                        Console.WriteLine("You need digits");
+                    }
+                    break;
+                default: 
+                    Console.WriteLine("Inccorect figure");
+                    break;
+            }
+            void line()
+            {
+                Console.Write("Input parameter: ");
+            }
+        }
+    }
+}
